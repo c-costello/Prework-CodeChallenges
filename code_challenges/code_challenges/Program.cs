d206214298b75428efc5aac291f5c9af4c58bba1
Clarice Costello <claricostello@outlook.com>
2019-01-04 14:07:08 -0800

    Created basic function outline using a set test array

--- code_challenges/code_challenges/Program.cs
+++ code_challenges/code_challenges/Program.cs
@@ -6,8 +6,29 @@ namespace code_challenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello");
+            int [] testArr = { 2, 2, 4, 5, 7 };
+            Console.WriteLine("testArr: " + String.Join(',', testArr));
+            Console.Write("Select a number between 1 and 10");
+            string inputStr = Console.ReadLine();
+            int input = Convert.ToInt32(inputStr);
+            Console.WriteLine("Your Number: " + input);
+            Console.WriteLine("Your Score is: " +findScore(testArr, input));
             Console.ReadLine();
+
+        }
+        private static int findScore(int[] numbers, int choice)
+        {
+            int counter = 0;
+            foreach (var number in numbers)
+            {
+                if (number == choice)
+                {
+                    counter++;
+                }
+            }
+            int result = counter * choice;
+            return result;
         }
 
     }
